8744e6ba2839f7ccaf3268961547449f3ab1f620
arielle1502 <arielle1502@yahoo.com>
2020-08-19 15:56:14 +1000

    set up 2 test routes
    installed body-parser, express, cors, morgan. set up /status and /register test routes. also installed axios into client

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,22 @@
-console.log("test server")
+const express = require ('express');
+const bodyParser = require ('body-parser');
+// cors allows us to make requests from other services since node out of teh box doesnt allow us to do that
+const cors = require ('cors');
+// morgan logs errors on server adn logs it to a text file
+const morgan = require ('morgan');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+// have to pass waht type of errors we want to log, combine will log warnigs and errors
+app.use(morgan('combine'));
+
+app.get('/status', (req, res) => {
+  res.send({message: 'hello world'});
+});
+
+app.post('/register', (req, res) => {
+  res.send({message: `hello ${req.body.email}`});
+});
+
+app.listen(process.env.PORT || 8081);
